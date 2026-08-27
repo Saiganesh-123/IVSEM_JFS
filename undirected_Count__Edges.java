78fd9e310fe2d47de547e2bddf18f342b9d8c8a2
Saiganesh <gunishettisaiganesh@gmail.com>
2026-08-27 22:46:25 +0530

    Fix inner loop to iterate over graph columns

--- undirected_Count__Edges.java
+++ undirected_Count__Edges.java
@@ -4,7 +4,7 @@ class undirected_Count__Edges{
         int graph[][]={{0,1,0,1,0},{1,0,1,1,0},{0,1,0,0,1},{1,1,0,0,1},{0,0,1,1,0}};
         int one_ctr=0;
         for(int i=0;i<graph.length;i++){
-            for(int j=0;j<graph.length;j++){
+            for(int j=0;j<graph[0].length;j++){
                 if(graph[i][j]==1){
                     one_ctr++;
                 }
